eb5c179661c42e411851e22bebda891230ec0bb3
medeor413 <me@medeor.me>
2018-07-17 11:43:03 -0400

    Type annotate the entire packet parser Probably not strictly necessary but hey, it certainly works.

--- starrypy/parser.py
+++ starrypy/parser.py
@@ -1,12 +1,15 @@
 import io
 import logging
 import struct
+import typing
 import zlib
 from binascii import hexlify, unhexlify
-from typing import Callable
+from typing import BinaryIO, Callable, List, Dict, Any
 from .enums import PacketType, WarpType, WarpWorldType, SystemLocationType
 
 parser_logger = logging.getLogger("starrypy.parser")
+
+JsonType = typing.Union[None, bool, int, float, str, List["JsonType"], Dict[str, "JsonType"]]
 
 # Some structs, pre-built for performance
 
@@ -29,15 +32,15 @@ struct_cache = {
 # Basic data type parsing
 
 
-def parse_byte(stream):
+def parse_byte(stream: typing.BinaryIO) -> int:
     return ord(stream.read(1))
 
 
-def build_byte(obj):
+def build_byte(obj: int) -> bytes:
     return obj.to_bytes(1, byteorder="big", signed=False)
 
 
-def parse_with_struct(stream: io.BytesIO, data_type: str):
+def parse_with_struct(stream: typing.BinaryIO, data_type: str) -> typing.Union[bool, int, float, tuple]:
     """
     This function takes an input stream and transforms it into a variety of data types, depending on data_type.
     :param stream: A stream object of raw bytes.
@@ -51,7 +54,7 @@ def parse_with_struct(stream: io.BytesIO, data_type: str):
     return unpacked
 
 
-def build_with_struct(obj, data_type: str):
+def build_with_struct(obj: typing.Union[bool, int, float, tuple], data_type: str) -> bytes:
     """
     This function takes a variety of simple input data types and turns them into bytes depending on data_type.
     :param obj: The object to be parsed. Should be one of [bool, int, float] or a tuple of int or float.
@@ -61,7 +64,7 @@ def build_with_struct(obj, data_type: str):
     return struct_cache[data_type].pack(obj)
 
 
-def parse_vlq(stream: io.BytesIO):
+def parse_vlq(stream: BinaryIO) -> int:
     value = 0
     while True:
         try:
@@ -74,7 +77,7 @@ def parse_vlq(stream: io.BytesIO):
     return value
 
 
-def build_vlq(obj: int):
+def build_vlq(obj: int) -> bytes:
     result = bytearray()
     value = int(obj)
     if obj == 0:
@@ -92,7 +95,7 @@ def build_vlq(obj: int):
     return bytes(result)
 
 
-def parse_signed_vlq(stream: io.BytesIO):
+def parse_signed_vlq(stream: BinaryIO) -> int:
     v = parse_vlq(stream)
     if (v & 1) == 0x00:
         return v >> 1
@@ -100,50 +103,50 @@ def parse_signed_vlq(stream: io.BytesIO):
         return -((v >> 1) + 1)
 
 
-def build_signed_vlq(obj: int):
+def build_signed_vlq(obj: int) -> bytes:
     value = abs(obj * 2)
     if obj < 0:
         value -= 1
     return build_vlq(value)
 
 
-def parse_byte_array(stream: io.BytesIO):
+def parse_byte_array(stream: BinaryIO) -> bytes:
     array_len = parse_vlq(stream)
     return stream.read(array_len)
 
 
-def build_byte_array(obj: bytes):
+def build_byte_array(obj: bytes) -> bytes:
     return build_vlq(len(obj)) + obj
 
 
-def parse_utf8_string(stream: io.BytesIO):
+def parse_utf8_string(stream: BinaryIO) -> str:
     return parse_byte_array(stream).decode("utf-8")
 
 
-def build_utf8_string(obj: str):
+def build_utf8_string(obj: str) -> bytes:
     return build_byte_array(obj.encode("utf-8"))
 
 
-def parse_string_set(stream: io.BytesIO):
+def parse_string_set(stream: BinaryIO) -> List[str]:
     set_len = parse_vlq(stream)
     return [parse_utf8_string(stream) for _ in range(set_len)]
 
 
-def build_string_set(obj: list):
+def build_string_set(obj: List[str]) -> bytes:
     res = b''
     res += build_vlq(len(obj))
     return res + b"".join(x.encode("utf-8") for x in obj)
 
 
-def parse_uuid(stream: io.BytesIO):
+def parse_uuid(stream: BinaryIO) -> bytes:
     return hexlify(stream.read(16))
 
 
-def build_uuid(obj: bytes):
+def build_uuid(obj: bytes) -> bytes:
     return unhexlify(obj)
 
 
-def parse_json(stream: io.BytesIO):
+def parse_json(stream: BinaryIO) -> JsonType:
     t = parse_byte(stream)
     if t == 1:
         # null
@@ -170,7 +173,7 @@ def parse_json(stream: io.BytesIO):
         raise ValueError(f"Json does not have type with index {t}!")
 
 
-def build_json(obj):
+def build_json(obj: JsonType) -> bytes:
     res = b""
     if obj is None:
         res += b"\x01"
@@ -191,52 +194,55 @@ def build_json(obj):
     return res
 
 
-def parse_json_array(stream: io.BytesIO):
+def parse_json_array(stream: BinaryIO) -> List[JsonType]:
     return parse_set(stream, parse_json)
 
 
-def build_json_array(obj: list):
+def build_json_array(obj: List[JsonType]) -> bytes:
     return build_set(obj, build_json)
 
 
-def parse_json_object(stream: io.BytesIO):
+def parse_json_object(stream: BinaryIO) -> Dict[str, JsonType]:
     return parse_hashmap(stream, parse_utf8_string, parse_json)
 
 
-def build_json_object(obj: dict):
+def build_json_object(obj: Dict[str, JsonType]) -> bytes:
     return build_hashmap(obj, build_utf8_string, build_json)
 
 # Higher-level data object parsing functions
 
 
-def parse_maybe(stream, data_type):
+def parse_maybe(stream: BinaryIO,
+                data_type: Callable[[BinaryIO], Any]) -> typing.Optional:
     if parse_with_struct(stream, "bool"):
         return data_type(stream)
     return None
 
 
-def build_maybe(obj, data_type: Callable):
+def build_maybe(obj: typing.Optional, data_type: Callable[[Any], bytes]) -> bytes:
     if obj is not None:
         return build_with_struct(True, "bool") + data_type(obj)
     return build_with_struct(False, "bool")
 
 
-def parse_set(stream: io.BytesIO, data_type: Callable):
+def parse_set(stream: BinaryIO, data_type: Callable[[BinaryIO], Any]) -> List:
     set_len = parse_vlq(stream)
     return [data_type(stream) for _ in range(set_len)]
 
 
-def build_set(obj: list, data_type: Callable):
+def build_set(obj: List, data_type: Callable[[Any], bytes]) -> bytes:
     res = build_vlq(len(obj))
     return res + b"".join(data_type(x) for x in obj)
 
 
-def parse_hashmap(stream: io.BytesIO, key_type: Callable, value_type: Callable):
+def parse_hashmap(stream: BinaryIO, key_type: Callable[[BinaryIO], typing.Hashable],
+                  value_type: Callable[[BinaryIO], Any]) -> Dict:
     map_len = parse_vlq(stream)
     return dict((key_type(stream), value_type(stream)) for _ in range(map_len))
 
 
-def build_hashmap(obj: dict, key_type: Callable, value_type: Callable):
+def build_hashmap(obj: Dict, key_type: Callable[[typing.Hashable], bytes],
+                  value_type: Callable[[Any], bytes]) -> bytes:
     res = build_vlq(len(obj))
     key_list = (key_type(x) for x in obj.keys())
     val_list = (value_type(x) for x in obj.values())
@@ -244,7 +250,7 @@ def build_hashmap(obj: dict, key_type: Callable, value_type: Callable):
     return b"".join(zip(key_list, val_list))
 
 
-def parse_chat_header(stream: io.BytesIO):
+def parse_chat_header(stream: BinaryIO) -> Dict:
     res = {"mode": parse_byte(stream)}
     if res["mode"] > 1:
         res["channel"] = parse_utf8_string(stream)
@@ -255,7 +261,7 @@ def parse_chat_header(stream: io.BytesIO):
     return res
 
 
-def build_chat_header(obj: dict):
+def build_chat_header(obj: Dict) -> bytes:
     res = build_byte(obj["mode"])
     if obj["mode"] > 1:
         res += build_utf8_string(obj["channel"])
@@ -265,20 +271,21 @@ def build_chat_header(obj: dict):
     return res
 
 
-def parse_celestial_coordinates(stream: io.BytesIO):
+def parse_celestial_coordinates(stream: BinaryIO) -> Dict:
     return {
         "coordinates": parse_with_struct(stream, "vec3i"),
         "planet": parse_with_struct(stream, "int32"),
         "satellite": parse_with_struct(stream, "int32")
     }
 
-def build_celestial_coordinates(obj: dict):
+
+def build_celestial_coordinates(obj: Dict) -> bytes:
     return b"".join((build_with_struct(obj["coordinates"], "vec3i"),
                      build_with_struct(obj["planet"], "int32"),
                      build_with_struct(obj["satellite"], "int32")))
 
 
-def parse_system_location(stream: io.BytesIO):
+def parse_system_location(stream: BinaryIO) -> Dict:
     dest_type = parse_byte(stream)
     res = {"type": dest_type}
     if dest_type == SystemLocationType.SYSTEM:
@@ -299,7 +306,7 @@ def parse_system_location(stream: io.BytesIO):
     return res
 
 
-def build_system_location(obj: dict):
+def build_system_location(obj: Dict) -> bytes:
     dest_type = obj["type"]
     res = build_byte(dest_type)
     if dest_type == SystemLocationType.SYSTEM:
@@ -323,7 +330,7 @@ def build_system_location(obj: dict):
     return res
 
 
-def parse_warp_action(stream: io.BytesIO):
+def parse_warp_action(stream: BinaryIO) -> Dict:
     warp_type = parse_byte(stream)
     res = {"warp_type": warp_type}
 
@@ -356,7 +363,7 @@ def parse_warp_action(stream: io.BytesIO):
     return res
 
 
-def build_warp_action(obj: dict):
+def build_warp_action(obj: Dict) -> bytes:
     warp_type = obj["warp_type"]
     res = build_byte(warp_type)
 
@@ -392,7 +399,7 @@ def build_warp_action(obj: dict):
     return res
 
 
-def parse_world_chunks(stream: io.BytesIO):
+def parse_world_chunks(stream: BinaryIO) -> Dict:
     # I'll be honest, I've not a damned clue what's going on in this thing.
     # And honestly, it's doubtful we'll need any more parsing than this;
     # Python is simply too slow for us to be parsing tile arrays and such
@@ -411,30 +418,30 @@ def parse_world_chunks(stream: io.BytesIO):
 # - Protocol request
 
 
-def parse_protocol_request(stream: io.BytesIO, _):
+def parse_protocol_request(stream: BinaryIO, _) -> Dict:
     return {"request_protocol_version": parse_with_struct(stream, "uint32")}
 
 # - Protocol response
 
 
-def parse_protocol_response(stream: io.BytesIO, _):
+def parse_protocol_response(stream: BinaryIO, _) -> Dict:
     return {"allowed": parse_with_struct(stream, "bool")}
 
 # Universe server to client
 # - Server disconnect
 
 
-def parse_server_disconnect(stream: io.BytesIO, _):
+def parse_server_disconnect(stream: BinaryIO, _) -> Dict:
     return {"reason": parse_utf8_string(stream)}
 
 
-def build_server_disconnect(obj: dict, _):
+def build_server_disconnect(obj: Dict, _) -> bytes:
     return build_utf8_string(obj["reason"])
 
 # - Connect success
 
 
-def parse_connect_success(stream: io.BytesIO, _):
+def parse_connect_success(stream: BinaryIO, _) -> Dict:
     return {
         "client_id": parse_vlq(stream),
         "server_uuid": parse_uuid(stream),
@@ -446,7 +453,7 @@ def parse_connect_success(stream: io.BytesIO, _):
     }
 
 
-def build_connect_success(obj: dict, _):
+def build_connect_success(obj: Dict, _) -> bytes:
     res = [
         build_vlq(obj["client_id"]),
         build_uuid(obj["server_uuid"]),
@@ -461,23 +468,23 @@ def build_connect_success(obj: dict, _):
 # - Connect failure
 
 
-def parse_connect_failure(stream: io.BytesIO, _):
+def parse_connect_failure(stream: BinaryIO, _) -> Dict:
     return {"reason": parse_utf8_string(stream)}
 
 
-def build_connect_failure(obj: dict, _):
+def build_connect_failure(obj: Dict, _) -> bytes:
     return build_utf8_string(obj["reason"])
 
 # - Handshake challenge
 
 
-def parse_handshake_challenge(stream: io.BytesIO, _):
+def parse_handshake_challenge(stream: BinaryIO, _) -> Dict:
     return {"password_salt": parse_byte_array(stream)}
 
 # - Chat received
 
 
-def parse_chat_received(stream: io.BytesIO, _):
+def parse_chat_received(stream: BinaryIO, _) -> Dict:
     return {
         "header": parse_chat_header(stream),
         "name": parse_utf8_string(stream),
@@ -486,7 +493,7 @@ def parse_chat_received(stream: io.BytesIO, _):
     }
 
 
-def build_chat_received(obj: dict, _):
+def build_chat_received(obj: Dict, _) -> bytes:
     res = build_chat_header(obj["header"])
     res += build_utf8_string(obj["name"])
     res += build_byte(obj["junk"])
@@ -496,17 +503,17 @@ def build_chat_received(obj: dict, _):
 # - Universe time update
 
 
-def parse_universe_time_update(stream: io.BytesIO, _):
+def parse_universe_time_update(stream: BinaryIO, _) -> Dict:
     return {"timestamp": parse_with_struct(stream, "double")}
 
 
-def build_universe_time_update(obj: dict, _):
+def build_universe_time_update(obj: Dict, _) -> bytes:
     return build_with_struct(obj["timestamp"], "double")
 
 # - Player warp result
 
 
-def parse_player_warp_result(stream: io.BytesIO, _):
+def parse_player_warp_result(stream: BinaryIO, _) -> Dict:
     return {
         "success": parse_with_struct(stream, "bool"),
         "warp_action": parse_warp_action(stream),
@@ -514,7 +521,7 @@ def parse_player_warp_result(stream: io.BytesIO, _):
     }
 
 
-def build_player_warp_result(obj: dict, _):
+def build_player_warp_result(obj: Dict, _) -> bytes:
     res = build_with_struct(obj["success"], "bool")
     res += build_warp_action(obj["warp_action"])
     return res + build_with_struct(obj["warp_action_invalid"], "bool")
@@ -523,7 +530,7 @@ def build_player_warp_result(obj: dict, _):
 # - Client connect
 
 
-def parse_client_connect(stream: io.BytesIO, _):
+def parse_client_connect(stream: BinaryIO, _) -> Dict:
     return {
         "assets_digest": parse_byte_array(stream),
         "allow_assets_mismatch": parse_with_struct(stream, "bool"),
@@ -546,53 +553,53 @@ def parse_client_connect(stream: io.BytesIO, _):
 # - Handshake response
 
 
-def parse_handshake_response(stream: io.BytesIO, _):
+def parse_handshake_response(stream: BinaryIO, _) -> Dict:
     return {"password_hash": parse_byte_array(stream)}
 
 # - Player warp-
 
 
-def parse_player_warp(stream: io.BytesIO, _):
+def parse_player_warp(stream: BinaryIO, _) -> Dict:
     return {
         "warp_action": parse_warp_action(stream),
         "deploy": parse_with_struct(stream, "bool")
     }
 
 
-def build_player_warp(obj: dict, _):
+def build_player_warp(obj: Dict, _) -> bytes:
     return build_warp_action(obj["warp_action"]) + build_with_struct(obj["deploy"], "bool")
 
 # - Fly ship
 
 
-def parse_fly_ship(stream: io.BytesIO, _):
+def parse_fly_ship(stream: BinaryIO, _) -> Dict:
     return {
         "system": parse_with_struct(stream, "vec3i"),
         "location": parse_system_location(stream)
     }
 
 
-def build_fly_ship(obj: dict, _):
+def build_fly_ship(obj: Dict, _) -> bytes:
     return build_with_struct(obj["system"], "vec3i") + build_system_location(obj["location"])
 
 # - Chat send
 
 
-def parse_chat_send(stream: io.BytesIO, _):
+def parse_chat_send(stream: BinaryIO, _) -> Dict:
     return {
         "text": parse_utf8_string(stream),
         "send_mode": parse_byte(stream)
     }
 
 
-def build_chat_send(obj: dict, _):
+def build_chat_send(obj: Dict, _) -> bytes:
     return build_utf8_string(obj["text"]) + build_byte(obj["send_mode"])
 
 # World server to client
 # - World Start
 
 
-def parse_world_start(stream: io.BytesIO, _):
+def parse_world_start(stream: BinaryIO, _) -> Dict:
     return {
         "template_data": parse_json(stream),
         "sky_data": parse_byte_array(stream),
@@ -612,7 +619,8 @@ def parse_world_start(stream: io.BytesIO, _):
 
 # - Give item
 
-def parse_give_item(stream: io.BytesIO, _):
+
+def parse_give_item(stream: BinaryIO, _) -> Dict:
     return {
         "name": parse_utf8_string(stream),
         "count": parse_vlq(stream),
@@ -620,14 +628,14 @@ def parse_give_item(stream: io.BytesIO, _):
     }
 
 
-def build_give_item(obj: dict, _):
+def build_give_item(obj: Dict, _) -> bytes:
     return build_utf8_string(obj["name"]) + build_vlq(obj["count"]) + build_json(obj["parameters"])
 
 # World bidirectional
 # - Step update
 
 
-def parse_step_update(stream: io.BytesIO, _):
+def parse_step_update(stream: BinaryIO, _) -> Dict:
     return {"remote_step": parse_with_struct(stream, "uint64")}
 
 # Parsing function dispatch thing
